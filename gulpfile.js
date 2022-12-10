0b4e26330ed451c5a45f81fc3a7d2680e6ec8226
HelloSkills <helloskillsweb@gmail.com>
2022-12-10 23:40:18 +0300

    Header Ready

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ let gulp = require('gulp'),
 
 gulp.task('sass', function () {
 return gulp.src('app/scss/**/*.scss')
-        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(sass({ outputStyle: 'expanded' }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(autoprefixer({
             overrideBrowserslist: ['last 8 version']
@@ -63,7 +63,7 @@ gulp.task('browser-sync', function() {
 
 
 gulp.task('watch', function (){
-    gulp.watch('app/scss/style.scss', gulp.parallel('sass'))
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
     gulp.watch('app/*.html', gulp.parallel('html'))
     gulp.watch('app/js/*.js', gulp.parallel('js'))
 });
