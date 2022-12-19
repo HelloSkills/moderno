260236b49d89d1dd4a60853cb6d5b81d1db2b214
HelloSkills <helloskillsweb@gmail.com>
2022-12-19 11:20:15 +0300

    SlickSlider Ready

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,20 @@
 $(function(){
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+      });
+
+$(".product-slider__inner").slick ({
+dots: true,
+arrows: false,
+slidesToShow: 4,
+slidesToScroll: 4,
+
+});
 
     var mixer = mixitup('.products__inner-box');
+
 
 });
