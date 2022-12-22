36c4b9cbf55422c3c952fbb9e09f58f5f0dd614d
HelloSkills <helloskillsweb@gmail.com>
2022-12-22 22:56:55 +0300

    ready advantages

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,5 @@
 $(function(){
 
-    $(".rate-star").rateYo({
-        rating: 5,
-        starWidth: "12px",
-        readOnly: true
-      });
-
 $(".product-slider__inner").slick ({
 dots: true,
 arrows: false,
