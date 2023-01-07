fc83808e56aef28c86e483ce67ae17001c9f6dbf
HelloSkills <helloskillsweb@gmail.com>
2023-01-07 22:57:10 +0300

    2 page worky

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,26 @@
-$(function(){
+$(function () {
 
-$(".product-slider__inner").slick ({
-dots: true,
-arrows: false,
-slidesToShow: 4,
-slidesToScroll: 4,
+    $(".product-slider__inner").slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
 
-});
+    });
+
+
+    
+    $("#demo_0").ionRangeSlider({
+        min: 100,
+        max: 1000,
+        from: 550
+    });
+    
+    
+    $("#example_id").ionRangeSlider();
 
     var mixer = mixitup('.products__inner-box');
+
 
 
 });
